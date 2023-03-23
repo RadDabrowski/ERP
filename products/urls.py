7578e8ddb2cb667d052f129870bc64416a808734
Radosław Dąbrowski <radoslaw.dabrowski@notbug.pl>
2023-03-23 17:25:16 +0100

    changed urls for categories and subcategories

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from products.views import SegmentListView, NewSegmentView
+from products.views import SegmentListView, NewSegmentView, CategoryListView, \
+    NewCategoryView, SubCategoryListView, NewSubCategoryView
 
 urlpatterns = [
     path('segment/', SegmentListView.as_view(), name='segment-list'),
     path('segment-new/', NewSegmentView.as_view(), name='new-segment'),
-    path('category/', SegmentListView.as_view(), name='category-list'),
-    path('category-new/', NewSegmentView.as_view(), name='new-category'),
-    path('subcategory/', SegmentListView.as_view(), name='subcategory-list'),
-    path('subcategory-new/', NewSegmentView.as_view(), name='new-subcategory'),
+    path('category/', CategoryListView.as_view(), name='category-list'),
+    path('category-new/', NewCategoryView.as_view(), name='new-category'),
+    path('subcategory/', SubCategoryListView.as_view(), name='subcategory-list'),
+    path('subcategory-new/', NewSubCategoryView.as_view(), name='new-subcategory'),
 ]
